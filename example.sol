f4038dd2667917cb50212318b6b9ccc16148a14e
Lefteris Karapetsas <lefteris@refu.co>
2016-09-14 22:54:56 +0200

    Sikorka contracts initial commit

---
+++ example.sol
@@ -0,0 +1,74 @@
+/**
+ * An example contract using the Sikorka API
+ *
+ * A shop owner wants to give some discount credit tokens to any users
+ * who go on location and interact with the contracts he/she has deployed.
+ *
+ * @author Lefteris Karapetsas
+ * @license BSD3
+ */
+
+pragma solidity ^0.4.1;
+
+import "./sikorka_basic_interface.sol";
+
+
+contract DiscountTokens is SikorkaBasicInterface {
+
+    mapping (address => uint256) balances;
+    address[] participants;
+    uint tokens_to_reward;
+    uint token_round_end;
+
+    /**
+     * Constructor
+     *
+     * @param _tokens_to_reward    The number of tokens to reward to each
+     *                             user who succesfully interacts with
+     *                             the contract
+     * @param _round_duration      The number of seconds this token round
+     *                             will last
+     */
+    function DiscountTokens(
+        uint _latitude,
+        uint _longtitude,
+        string _question,
+        bytes32 _answer_hash,
+        uint _tokens_to_reward,
+        uint _round_duration
+    ) SikorkaBasicInterface(
+        _latitude,
+        _longtitude,
+        _question,
+        _answer_hash) {
+
+        tokens_to_reward = _tokens_to_reward;
+        token_round_end = now + _round_duration;
+    }
+
+    /**
+     * Claim discount tokens for the shop!
+     *
+     * @param _answer        The answer to the challenge question.
+     */
+    function claimToken(string _answer) need_pop(_answer) {
+
+        // User already got their discount tokens for this round
+        if (balances[msg.sender] != 0) {
+            return;
+        }
+        balances[msg.sender] += tokens_to_reward;
+    }
+
+    /**
+     * Clear out the tokens to allow for a new round of discounts
+     *
+     * @param _new_duration        The new duration in seconds
+     */
+    function clearTokens(uint _new_duration) only_owner {
+        for (uint i = 0; i < participants.length; i ++) {
+            balances[participants[i]] = 0;
+        }
+        delete participants;
+    }
+}
---
+++ sikorka_basic_interface.sol
@@ -0,0 +1,74 @@
+/**
+ * The Sikorka Basic Interface contract.
+ *
+ * All contracts using Sikorka should derive from it and
+ * extend it so they can be used by the applications around it.
+ *
+ * @author Lefteris Karapetsas
+ * @license BSD3
+ */
+
+pragma solidity ^0.4.1;
+
+contract Owned {
+    address owner;
+
+    /// Allows only the owner to call a function
+    modifier only_owner { if (msg.sender != owner) {
+            throw;
+        }
+        _;
+    }
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+
+    function change_owner(address _newOwner) only_owner {
+        owner = _newOwner;
+    }
+}
+
+contract SikorkaBasicInterface is Owned {
+
+    string public question;
+    bytes32 internal answer_hash;
+    uint internal latitude;
+    uint internal longtitude;
+
+    /**
+     * Require Proof Of Presence for the function to be executed
+     *
+     * @param _answer        The answer to give to the challenge question
+     */
+    modifier need_pop(string _answer) {
+        if (sha3(_answer) != answer_hash) {
+            throw;
+        }
+        _;
+    }
+
+    function SikorkaBasicInterface(
+        uint _latitude,
+        uint _longtitude,
+        string _question,
+        bytes32 _answer_hash
+    ) {
+        latitude = _latitude;
+        longtitude = _longtitude;
+        question = _question;
+        answer_hash = _answer_hash;
+    }
+
+    /**
+     * Change the challenge question/answer combination
+     *
+     * @param _question        The new question
+     * @param _answer_hash     The hash to the answer
+     */
+    function change_question(string _question, bytes32 _answer_hash) only_owner {
+        question = _question;
+        answer_hash = _answer_hash;
+    }
+}
