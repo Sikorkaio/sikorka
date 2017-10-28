3320ddd1bcf682ed3b764c944018c26c596f9b21
Lefteris Karapetsas <lefteris@refu.co>
2017-10-28 13:26:28 +0200

    Send hex string via QR code

--- sikorka/qrcodes.py
+++ sikorka/qrcodes.py
@@ -1,6 +1,7 @@
 import gevent
 import time
 import os
+import binascii
 import qrcode
 from qrcode.image.pure import PymagingImage
 
@@ -11,7 +12,10 @@ def generate_qr_codes(end_event, account):
         signed_bytes = account.create_qr_sign(int(time.time()))
         signed_bytes = bytearray.fromhex('03') + signed_bytes
 
-        img = qrcode.make(signed_bytes, image_factory=PymagingImage)
+        img = qrcode.make(
+            b'0x' + binascii.hexlify(signed_bytes),
+            image_factory=PymagingImage
+        )
         rootpath = os.path.dirname(os.path.abspath(__file__))
 
         with open(os.path.join(rootpath, 'ui', 'static', 'qrcode.png'), 'wb') as f:
